1ee777392a60465542228364fed3dac8bb150d6c
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-08-17 12:21:29 +0900

    commit-01

--- m23_xgb1_save_model.py
+++ m23_xgb1_save_model.py
@@ -1,83 +1,42 @@
-from sklearn import datasets
-from xgboost import XGBRegressor
-from sklearn.datasets import load_boston
+
+from xgboost import XGBClassifier, XGBRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_boston
 import numpy as np
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+
+# 1-1. data
+datasets = load_boston()
 
 # 1. data
+x_data = datasets["data"] # (506, 13) 
+y_data = datasets["target"] # (506,)
 
-datasets = load_boston()
-x = datasets['data']
-y = datasets['target']
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data,
+      test_size=0.25, shuffle=True, random_state=66)
 
-print(x.shape, y.shape) # (506, 13) (506,)
+# print(x_train.shape, x_test.shape) # (404, 13) (102, 13)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y,
-        train_size=0.8,test_size=0.2, shuffle=True, random_state=66)
-
-scaler = MinMaxScaler() 
-scaler.fit(x_train) 
-x_train = scaler.transform(x_train) 
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-# # 2. model 
-# model = XGBRegressor(n_estimators = 1000, learning_rate=0.1, n_jobs=1)
+model = XGBRegressor(n_estimators=100, learning_rate=0.052,
+                     n_jobs=-1)
 
-# # 3. fit
-# model.fit(x_train, y_train, verbose=1, eval_metric='rmse', # ['mae','logloss']
-#                 eval_set=[(x_train, y_train), (x_test, y_test)],
-#                 early_stopping_rounds=10) 
-# # 4. eval
-# results = model.score(x_test, y_test)
-# print("result : ", results)
+model.fit(x_train, y_train, verbose=1, 
+            early_stopping_rounds=20,
+            # eval_metric=['rmse', 'mae', 'logloss'],
+            eval_set=[(x_train, y_train), (x_test, y_test)]
+            # train set 명시해야 validation 지정 가능
+)
 
-# y_pred = model.predict(x_test)
-# r2 = r2_score(y_test, y_pred)
-# print("r2 score : ", r2)
+model.save_model('./_save/XGB_save/M23_save.dat')
 
-'''
-1. xgb defalut
-result :  0.8561620022190288
-r2 score :  0.8561620022190288
-
-2. xgb tune
-result :  0.9314278953418559
-r2 score :  0.9314278953418559
-'''
-
-# print("=======================================")
-# hist = model.evals_result()
-# print(hist)
-
-# 저장
-# import pickle
-# pickle.dump(model, open('./_save/xgb_save/m21_pickle.dat', 'wb'))
-
-import joblib
-# joblib.dump(model, './_save/xgb_save/m21.joblib.dat')
-model = joblib.load('./_save/xgb_save/m21.joblib.dat')
-
-# 4. eval
-results = model.score(x_test, y_test)
-print("result : ", results)
+result = model.score(x_test, y_test)
+print('result :', result)
 
 y_pred = model.predict(x_test)
 r2 = r2_score(y_test, y_pred)
-print("r2 score : ", r2)
-
-'''
-1. xgb defalut
-result :  0.8561620022190288
-r2 score :  0.8561620022190288
-
-2. xgb tune
-result :  0.9314278953418559
-r2 score :  0.9314278953418559
-'''
-
-print("=======================================")
-hist = model.evals_result()
-print(hist)
+print('r2 score :', r2)
